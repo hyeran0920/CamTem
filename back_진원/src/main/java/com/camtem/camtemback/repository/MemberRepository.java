cbc6ea50688b3d72579dce3de6bd9617980cf082
jinwonsong <jinwonsong@naver.com>
2024-05-09 13:35:57 +0900

    의성+진원

--- back_진원/src/main/java/com/camtem/camtemback/repository/MemberRepository.java
+++
@@ -1,8 +0,0 @@
-package com.camtem.camtemback.repository;
-
-import com.camtem.camtemback.dto.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-
-}
