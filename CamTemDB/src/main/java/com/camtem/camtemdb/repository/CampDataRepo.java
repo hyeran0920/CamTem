2345b43c7094bb72fc2f6cae537d5ab9d7844f3d
jiyeon <qkrwluds7998@gmail.com>
2024-05-16 18:13:31 +0900

    delete all folers

--- CamTemDB/src/main/java/com/camtem/camtemdb/repository/CampDataRepo.java
+++
@@ -1,8 +0,0 @@
-package com.camtem.camtemdb.repository;
-
-
-import com.camtem.camtemdb.entity.Camping;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CampDataRepo extends JpaRepository<Camping,Integer> {
-}
